655ae5b3297cdaed68a53ae65ef33ca871eb2be9
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-31 21:21:42 +0530

    RideFlux: api-gateway: completed

---
+++ api-gateway/src/main/java/com/ranjankumarmandal/api_gateway/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.ranjankumarmandal.api_gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(
+            ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex ->
+                        ex.anyExchange().permitAll())
+                .build();
+    }
+}
