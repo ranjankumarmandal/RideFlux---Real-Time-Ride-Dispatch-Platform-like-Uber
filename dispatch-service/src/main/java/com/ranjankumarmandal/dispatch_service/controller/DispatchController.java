8a1302509efbed88f1b9840ae63b5adf7485ba08
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-31 16:49:41 +0530

    RideFlux: dispatch-service: completed controller

---
+++ dispatch-service/src/main/java/com/ranjankumarmandal/dispatch_service/controller/DispatchController.java
@@ -0,0 +1,12 @@
+// For debugging purpose only
+
+@RestController
+@RequestMapping("/debug/dispatch")
+public class DispatchController {
+
+    private final DispatchService dispatchService;
+
+    public DispatchController(DispatchService dispatchService) {
+        this.dispatchService = dispatchService;
+    }
+}
