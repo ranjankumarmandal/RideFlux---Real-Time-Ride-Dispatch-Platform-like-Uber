faf3152772c45326c1a44b56306607cf60593378
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-31 16:12:10 +0530

    RideFlux: driver-service: completed intial setup

---
+++ driver-service/src/test/java/com/ranjankumarmandal/driver_service/DriverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjankumarmandal.driver_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
